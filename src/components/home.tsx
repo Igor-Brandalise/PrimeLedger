a7a695890458a8393656e62b42e3092543487889
Igor Brandalise <brandaliseigor123@gmail.com>
2025-11-27 16:44:50 -0300

    feat: importando os componentes e passando as props

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import { Entradas } from "./entradas";
+import { Saidas } from "./saidas";
+import { Saldo } from "./saldo";
 
 export function Home() {
+  const [salario, setSalario] = useState("");
+  const [saidas, setSaidas] = useState("");
+
   return (
-    <main className="">
-      <Entradas />
+    <main className="flex items-center justify-center gap-8">
+      <Entradas salario={salario} setSalario={setSalario} />
+
+      <Saidas saidas={saidas} setSaidas={setSaidas} />
+
+      <Saldo salario={salario} saidas={saidas} />
     </main>
   );
 }

118c354e9b7df50a4d5ac29aff69e5eafd9ff133
Igor Brandalise <brandaliseigor123@gmail.com>
2025-11-29 23:39:43 -0300

    feat: colunas para mobile

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,18 +2,21 @@ import { useState } from "react";
 import { Entradas } from "./entradas";
 import { Saidas } from "./saidas";
 import { Saldo } from "./saldo";
+import { Transacao } from "./transacao";
 
 export function Home() {
   const [salario, setSalario] = useState("");
   const [saidas, setSaidas] = useState("");
 
   return (
-    <main className="flex items-center justify-center gap-8">
+    <main className="flex flex-col items-center justify-center gap-8">
       <Entradas salario={salario} setSalario={setSalario} />
 
       <Saidas saidas={saidas} setSaidas={setSaidas} />
 
       <Saldo salario={salario} saidas={saidas} />
+
+      <Transacao />
     </main>
   );
 }

6d68c3b712dd3549e3c6e209c6a8a2f9ccc67b39
Igor Brandalise <brandaliseigor123@gmail.com>
2026-02-01 00:32:17 -0300

    h-screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './index.css'
 
 function App() {
   return(
-    <div className='bg-[#121212] h-full'>
+    <div className='bg-[#121212] h-screen'>
       <Header />
       <Home />
     </div>

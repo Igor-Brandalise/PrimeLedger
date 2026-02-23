199899cc754aaaa352da6af2fa9b71fb68c663ca
Igor-Brandalise <brandaliseigor123@gmail.com>
2026-02-22 22:14:59 -0300

    alinhando os inputs de saidas e saldo

--- src/components/saldo.tsx
+++ src/components/saldo.tsx
@@ -36,20 +36,22 @@ export function Saldo({ salario, saidas }: SaldoProps) {
   let final = formatarNumero(total);
 
   return (
-    <div className="relative mt-10 items-center flex">
-      <span className="absolute left-3 top-1/2 -translate-y-1/2 text-black font-extrabold">
-        R$
-      </span>
-
+    <div className=" ">
       <Saidas gasto={valorGasto} />
 
-      <input
-        type="text"
-        value={final}
-        readOnly
-        placeholder="Saldo..."
-        className="no-spinner rounded-[10px] p-2 border-2 bg-zinc-400 border-green-600 pl-10 items-center  "
-      />
+      <div className="relative">
+        <span className="absolute left-3 top-1/2 -translate-y-1/2 text-black font-extrabold">
+          R$
+        </span>
+
+        <input
+          type="text"
+          value={final}
+          readOnly
+          placeholder="Saldo..."
+          className="no-spiner w-full rounded-[10px] border-2 border-green-600 bg-zinc-400 pl-10 py-2 text-black"
+        />
+      </div>
     </div>
   );
 }
